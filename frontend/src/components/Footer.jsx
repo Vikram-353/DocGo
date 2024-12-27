c9d9e0a64f598733b1aa02bfdb5b4a3dde9d1625
Vikram <vikramranjan71122@gmail.com>
2024-12-27 21:54:59 +0530

    Filter Doctors

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
+import {  useNavigate } from 'react-router-dom'
+
 import { assets } from '../assets/assets_frontend/assets'
 
 function Footer() {
+const navigate=useNavigate(); 
+
   return (
     <div className='md:mx-10'>
-        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+        <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm '>
         {/* ---------Left Side---------- */}
 
         <div>
-            <img className='mb-5 w-40' src={assets.logo2} alt="" />
+            <img onClick={()=>navigate('/')}  className='mb-5 w-40 cursor-pointer' src={assets.logo2} alt="" />
             <p className='w-full md:w-2/3 text-gray-600 leading-6'>
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
     const [token,SetToken]=useState(true);
   return (
     <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'> 
-        <img  className='w-44 cursor-pointer' src={assets.logo2} alt="" />
+        <img onClick={()=>navigate('/')} className='w-44 cursor-pointer' src={assets.logo2} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium'>
             <NavLink to='/'>
                 <li className='py-4'>Home</li>
@@ -34,7 +34,7 @@ function Navbar() {
             {
             token ? 
             <div className='flex items-center gap-2 cursor-pointer group relative'>
-                <img className='w-8 rounded-full ' src={assets.profile_pic} alt="" />
+                <img  className='w-8 rounded-full ' src={assets.profile_pic} alt="" />
                  <img className='w-2.5' src={assets.dropdown_icon} alt="" />
                  <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                     <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
