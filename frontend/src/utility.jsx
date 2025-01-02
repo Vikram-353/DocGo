b47f19da27d9eb15692c4864b1e841378678a93f
Vikram <vikramranjan71122@gmail.com>
2025-01-02 21:52:56 +0530

    My Profile

--- frontend/src/utility.jsx
+++ frontend/src/utility.jsx
@@ -1,19 +1,19 @@
 // index.js or routes.js
 
-import Doctors from './pages/Doctors';
-import Appointment from './pages/Appointment';
-import About from './pages/About';
-import Login from './pages/Login';
-import Contact from './pages/Contact';
-import MyAppointment from './pages/MyAppointment';
-import MyProfile from './pages/MyProfile';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import SpecialityMenu from './components/SpecialityMenu';
-import TopDoctors from './components/TopDoctors';
-import Banner from './components/Banner';
-import Footer from './components/Footer';
-import RelatedDoctors from './components/RelatedDoctors';
+import Doctors from "./pages/Doctors";
+import Appointment from "./pages/Appointment";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import MyAppointment from "./pages/MyAppointment";
+import MyProfile from "./pages/MyProfile";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import SpecialityMenu from "./components/SpecialityMenu";
+import TopDoctors from "./components/TopDoctors";
+import Banner from "./components/Banner";
+import Footer from "./components/Footer";
+import RelatedDoctors from "./components/RelatedDoctors";
 
 export {
   Doctors,
@@ -28,5 +28,6 @@ export {
   SpecialityMenu,
   TopDoctors,
   Banner,
-  Footer,RelatedDoctors
+  Footer,
+  RelatedDoctors,
 };
