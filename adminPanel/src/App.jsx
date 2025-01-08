67e0377f904f4e4eeff48992736fac9a8b140db0
Vikram <vikramranjan71122@gmail.com>
2025-01-08 22:05:57 +0530

    DashBoard

--- adminPanel/src/App.jsx
+++ adminPanel/src/App.jsx
@@ -3,6 +3,12 @@ import Login from "./pages/Login";
 import { ToastContainer } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./component/Navbar";
+import SideBar from "./component/SideBar";
+import { Routes, Route } from "react-router-dom";
+import DashBoard from "./pages/Admin/DashBoard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorList from "./pages/Admin/DoctorList";
 function App() {
   const { atoken } = useContext(AdminContext) || {};
 
@@ -10,6 +16,16 @@ function App() {
     <div className="bg-[#F8F9FD)]">
       <ToastContainer />
       <Navbar />
+      <div className="flex items-start">
+        <SideBar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<DashBoard />} />
+          <Route path="/all-appointments" element={<AllAppointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
